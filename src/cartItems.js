c69242a27861daf5a74f9991be06534eb363b511
Yan Lima Barbosa <yanbr763@gmail.com>
2024-06-26 23:55:08 -0300

    fix: image source change

--- src/cartItems.js
+++ src/cartItems.js
@@ -1,31 +1,31 @@
 const cartItems = [
   {
-    id: 'rec1JZlfCIBOPdcT2',
-    title: 'Samsung Galaxy S8',
-    price: '399.99',
-    img: 'https://images2.imgbox.com/c2/14/zedmXgs6_o.png',
+    id: "rec1JZlfCIBOPdcT2",
+    title: "Samsung Galaxy S8",
+    price: "399.99",
+    img: "https://t.ctcdn.com.br/oE6GINhDlQ1YznDUmUlqEWgoDaw=/1920x1080/smart/i692456.png",
     amount: 1,
   },
   {
-    id: 'recB6qcHPxb62YJ75',
-    title: 'google pixel',
-    price: '499.99',
-    img: 'https://images2.imgbox.com/fb/3d/O4TPmhlt_o.png',
+    id: "recB6qcHPxb62YJ75",
+    title: "google pixel",
+    price: "499.99",
+    img: "https://img.odcdn.com.br/wp-content/uploads/2023/04/shutterstock_2067563567.jpg",
     amount: 1,
   },
   {
-    id: 'recdRxBsE14Rr2VuJ',
-    title: 'Xiaomi Redmi Note 2',
-    price: '699.99',
-    img: 'https://images2.imgbox.com/4f/3d/WN3GvciF_o.png',
+    id: "recdRxBsE14Rr2VuJ",
+    title: "Xiaomi Redmi Note 2",
+    price: "699.99",
+    img: "https://t2.tudocdn.net/149205?w=1920",
     amount: 1,
   },
   {
-    id: 'recwTo160XST3PIoW',
-    title: 'Samsung Galaxy S7',
-    price: '599.99 ',
-    img: 'https://images2.imgbox.com/2e/7c/yFsJ4Zkb_o.png',
+    id: "recwTo160XST3PIoW",
+    title: "Samsung Galaxy S7",
+    price: "599.99 ",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHOqGAy_enr1S8kd7y3UyYp0SGeBrLSvCTwA&s",
     amount: 1,
   },
-];
-export default cartItems;
+]
+export default cartItems
