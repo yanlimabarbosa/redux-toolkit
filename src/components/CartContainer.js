3af0eb22b621a58c9f5b83c46145511d25901cd3
Yan Lima Barbosa <yanbr763@gmail.com>
2024-06-27 22:39:34 -0300

    Reducers - Return State

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,10 @@
 import CartItem from "./CartItem"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { clearCart } from "../features/cart/cartSlice"
 
 const CartContainer = () => {
+  const dispatch = useDispatch()
+
   const { cartItems, total, amount } = useSelector((state) => state.cart)
 
   if (amount < 1) {
@@ -32,7 +35,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   )
